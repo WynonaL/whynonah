d7c3b3ef85db684c6034bcf74735f7e1c24eb29c
Wynona Lam <wynonalam888@gmail.com>
2025-03-09 18:52:44 -0500

    added new bio

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,7 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm Wynona. I recently graduated from DePaul University with a B.S in Data Science. During my undegrad time, I ran a ecommerce business.`}
       </p>
       <div className="my-8">
         <BlogPosts />
